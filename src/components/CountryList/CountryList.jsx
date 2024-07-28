37417e9b004035328ad9dc96e1b2db844135ff00
NataliiaBoh <homesweethome160126@gmail.com>
2024-07-28 17:13:24 +0300

    add Country

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
-import { Grid, GridItem } from "..";
+import { Link } from 'react-router-dom';
+import { Grid, GridItem } from '..';
 
-export const CountryList = ({countries}) => {
-  return <Grid>
-    {countries.map((country) => <GridItem key={country.id}>
-  <Link >
-    <img src={country.flag} alt={country.country} />
-  </Link>
-</GridItem>)}
-  </Grid>;
+export const CountryList = ({ countries }) => {
+  return (
+    <Grid>
+      {countries.map(country => (
+        <GridItem key={country.id}>
+          <Link to={`/country/${country.id}`}>
+            <img src={country.flag} alt={country.country} />
+          </Link>
+        </GridItem>
+      ))}
+    </Grid>
+  );
 };
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,29 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryInfo, Heading, Section } from 'components';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const [error, setError] = useState(null);
+  const [country, setCountry] = useState({});
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const country = await fetchCountry(countryId);
+        setCountry(country);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    getData();
+  }, [countryId]);
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        {error && <Heading title={error} bottom />}
+        <CountryInfo country={country} />
       </Container>
     </Section>
   );
